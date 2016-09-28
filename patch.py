3a8ff304e18b3f4b9c039ef7e99f7519fe8805e9
Vincent <nattybear@naver.com>
2016-09-28 19:48:12 +0900

    change the way that write patch file

--- patch.py
+++ patch.py
@@ -24,8 +24,20 @@ fp2 = open(ORIGIN, 'rb')
 # 패치 적용 파일 만들기
 fp3 = open(NEW, 'wb')
 
+# 파일 크기 구하기
+fsize = getsize(ORIGIN)
+
+# 텍스트 전체 길이 구하기
+fp2.seek(int('18', 16))
+tsize = fp2.read(4)
+tsize = unpack('I', tsize)[0]
+
+# 텍스트 시작 위치 구하기
+tstart = fsize - tsize
+
 # 패치 적용 파일에 원본 파일 쓰기
-fp3.write(fp2.read())
+fp2.seek(0)
+fp3.write(fp2.read(tstart - 1))
 
 # 패치 크기 가공하기
 psize = pack('I', psize)
@@ -34,17 +46,11 @@ psize = pack('I', psize)
 fp3.seek(int('18', 16))
 fp3.write(psize)
 
-# 텍스트 전체 길이 구하기
-fp2.seek(int('18', 16))
-tsize = fp2.read(4)
-tsize = unpack('I', tsize)[0]
-
-# 파일 크기 구하기
-fsize = getsize(ORIGIN)
-
-# 텍스트 시작 위치 구하기
-tstart = fsize - tsize
-
 # 패치 파일 쓰기
 fp3.seek(tstart)
 fp3.write(patch)
+
+# 파일 닫기
+fp.close()
+fp2.close()
+fp3.close()
